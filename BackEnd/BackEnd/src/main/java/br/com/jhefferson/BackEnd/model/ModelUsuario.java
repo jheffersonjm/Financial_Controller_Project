519c585a9af844c195d83ac5862961a3a8c96755
Jhefferson Dcosta <jheffersonmatheus2017@gmail.com>
2026-02-28 20:39:34 -0300

    Fianalisação do model

---
+++ BackEnd/BackEnd/src/main/java/br/com/jhefferson/BackEnd/model/ModelUsuario.java
@@ -0,0 +1,46 @@
+package br.com.jhefferson.backend.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+ @Table(name= "Usuario")
+public class ModelUsuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "nome", nullable = false, length = 100)
+    private String nome;
+    private String senha; 
+    private String email;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+}
